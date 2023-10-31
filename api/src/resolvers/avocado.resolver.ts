6a318f159eb380e0c32536d67305c42569065ae4
pacodinucci <franciscoldinucci@gmail.com>
2023-10-31 10:26:04 -0300

    changes

---
+++ api/src/resolvers/avocado.resolver.ts
@@ -0,0 +1,65 @@
+import { createHash } from 'crypto'
+import type { Avocado, Attributes, PrismaClient, Prisma } from "@prisma/client";
+
+type ResolverContext = {
+  orm: PrismaClient
+}
+
+export function findAll(parent: unknown, arg: {skip?: number, take?: number, where: Prisma.AvocadoWhereInput}, context: ResolverContext): Promise<Avocado[]> {
+  return context.orm.avocado.findMany({
+    include: { attributes: true },
+    skip: arg.skip,
+    take: arg.take,
+    where: arg.where
+  })
+}
+
+export function findOne(parent: unknown, args: { id: string }, context: ResolverContext): Promise<Avocado | null> {
+  return context.orm.avocado.findUnique({
+    where: { id: parseInt(args.id, 10) },
+    include: { attributes: true }
+  })
+}
+
+export const resolver: Record<
+  keyof (Avocado & { attributes: Attributes }),
+  (parent: Avocado & { attributes: Attributes }) => unknown> = {
+  id: (parent) => parent.id,
+  createdAt: (parent) => parent.createdAt,
+  updatedAt: (parent) => parent.updatedAt,
+  deletedAt: (parent) => parent.deletedAt,
+  sku: (parent) => parent.sku,
+  name: (parent) => parent.name,
+  price: (parent) => parent.price,
+  image: (parent) => parent.image,
+  attributes: (parent) => ({
+    description: parent.attributes.description,
+    shape: parent.attributes.shape,
+    hardiness: parent.attributes.hardiness,
+    taste: parent.attributes.taste,
+  }),
+}
+
+export function createAvo(
+  parent: unknown,
+  {
+    data,
+  }: { data: Pick<Avocado, 'name' | 'price' | 'image'> & Attributes },
+  context: ResolverContext
+): Promise<Avocado> {
+  const { name, price, image, ...attributes } = data;
+
+  return context.orm.avocado.create({
+    data: {
+      name,
+      price,
+      image,
+      sku: new Date().getTime().toString(), // Esta es una solucion rapida. El sku se deberia pedir en el input de graphql.
+      attributes: {
+        create: {
+          ...attributes,
+        }
+      }
+    }
+  })
+}
